8a4218521369ecabb024855dfb4b4ad588a90a70
danaFlorescu <mariadana977@gmail.com>
2023-02-10 14:26:09 +0200

    added weather data api http get

--- lib/screens/loading_screen.dart
+++ lib/screens/loading_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:clima/services/location.dart';
+import 'package:http/http.dart';
 
 class LoadingScreen extends StatefulWidget {
   @override
@@ -21,10 +22,20 @@ class _LoadingScreenState extends State<LoadingScreen> {
     print(latitude.toString() + '       ' + longitude.toString());
   }
 
+  void getData() async {
+    Response response = await get(
+      Uri.parse(
+          'https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99&appid=ebef5a8eb94489f1bf659f71896acee3'),
+    );
+
+    print(response.statusCode);
+  }
+
   @override
   void initState() {
     super.initState();
-    getLocation();
+    //getLocation();
+    getData();
   }
 
   @override
